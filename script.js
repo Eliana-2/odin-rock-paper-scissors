59b7cb1f74d4eea240b8b34084cce2d7d3d6954e
Eliana-2 <eliana.guizar1@gmail.com>
2022-05-18 18:27:42 -0700

    Create function that plays a round of Rock Paper Scissors using computer and player selections

--- script.js
+++ script.js
@@ -7,14 +7,42 @@ function computerPlay()
   switch(randInt)
   {
     case 0:
-      return 'Rock';
+      return `Rock`;
     case 1:
-      return 'Paper';
+      return `Paper`;
     case 2:
-      return 'Scissors';
+      return `Scissors`;
     default:
-      return 'Error';
+      return `Error`;
   }
 }
 
+//function that plays a round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection)
+{
+  //convert to lowercase to remain case insensitive
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
+  //compare selections
+  if(playerSelection === computerSelection)
+  {
+    return `It's a draw!`;
+  }
+  else
+  {
+    switch(playerSelection)
+    {
+      case `rock`:
+        return (computerSelection === `paper`) ? `You Lose! Paper beats Rock` : `You Win! Rock beats Scissors`;
+      case `scissors`:
+        return (computerSelection === `rock`) ? `You Lose! Rock beats Scissors` : `You Win! Scissors beats Paper`;
+      case `paper`:
+        return (computerSelection === `scissors`) ? `You Lose! Scissors beats Paper` : `You Win! Paper beats Rock`;
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
